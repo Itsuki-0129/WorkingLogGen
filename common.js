2079a9f6efe356c8fa17650586358e2518549aef
ItsukiNagao <nagaoituki@gmail.com>
2023-01-29 23:11:39 +0900

    経過日数を追加

--- common.js
+++ common.js
@@ -11,16 +11,16 @@ function calc() {
     let scheduledResult = "";
     let actualHours = 0;
     let svcHours = 0;
-    let baseHours = Number(document.getElementById('baseDays').value) * 8;
+    let elapsedHours = Number(document.getElementById('elapsedDays').value) * 8;
     let absencesHours = Number(document.getElementById('absences').value) * 8;
     let scheOverTimes = Number(document.getElementById('scheduledOverTimes').value);
     let overTimes = Number(document.getElementById('overTimes').value);
-    console.log(typeof(baseHours) + ";" + baseHours);
+    console.log(typeof(elapsedHours) + ";" + elapsedHours);
     console.log(typeof(absencesHours) + ":" + absencesHours);
     console.log(typeof(overTimes) + ":" + overTimes);
-    actualHours = baseHours - absencesHours + overTimes;
-    svcHours = baseHours + overTimes;
-    totalResult = "累計：" + actualHours + "（" + Number(actualHours - baseHours) + "）／" + svcHours + "（" + Number(overTimes) + "）";
-    scheduledResult = "予定：" + Number(baseHours + scheOverTimes) + "（" + Number(scheOverTimes) + "）／" + Number(baseHours + scheOverTimes) + "（" + Number(scheOverTimes) + "）";
+    actualHours = elapsedHours - absencesHours + overTimes;
+    svcHours = elapsedHours + overTimes;
+    totalResult = "累計：" + actualHours + "（" + Number(actualHours - elapsedHours) + "）／" + svcHours + "（" + Number(overTimes) + "）";
+    scheduledResult = "予定：" + Number(elapsedHours + scheOverTimes) + "（" + Number(scheOverTimes) + "）／" + Number(elapsedHours + scheOverTimes) + "（" + Number(scheOverTimes) + "）";
     document.getElementById('result').textContent=totalResult + "\n" +scheduledResult;
 }
